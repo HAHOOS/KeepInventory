d8f63995ac169a00b800774f8037e7841f26e737
HAHOOS <hahoos@hahoos.pl>
2024-11-13 16:18:12 +0100

    Add project files.

---
+++ KeepInventory/Main.cs
@@ -0,0 +1,106 @@
+﻿using MelonLoader;
+using BoneLib;
+using BoneLib.BoneMenu;
+using UnityEngine;
+using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Warehouse;
+using Il2CppSLZ.Marrow.Pool;
+
+[assembly: MelonInfo(typeof(global::HAHOOS.KeepInventory.Main), "Keep Inventory", "1.0.0", "HAHOOS")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonAuthorColor(0,255,165,0)]
+[assembly: MelonColor(0, 255, 72, 59)]
+
+namespace HAHOOS.KeepInventory
+{
+    public class Main : MelonMod
+    {
+
+        public Dictionary<string, Barcode> slots = new Dictionary<string, Barcode>();
+        private bool IsDebug = true;
+
+        public override void OnInitializeMelon()
+        {
+           LoggerInstance.Msg("Setting up KeepInventory");
+           SetupMenu();
+
+           Hooking.OnLevelLoaded += LevelLoadedEvent;
+           Hooking.OnLevelUnloaded += LevelUnloadedEvent;
+
+        }
+
+        private void LevelUnloadedEvent()
+        {
+            if (Settings.Enabled)
+            {
+                slots.Clear();
+                LoggerInstance.Msg("Saving inventory...");
+                if (Player.RigManager == null)
+                {
+                     LoggerInstance.Msg("RigManager does not exist");
+                    return;
+                }
+                if (Player.RigManager.inventory == null)
+                {
+                    LoggerInstance.Msg("Inventory does not exist");
+                    return;
+                }
+                foreach (var item in Player.RigManager.inventory.bodySlots)
+                {
+                    if (item.inventorySlotReceiver != null)
+                    {
+                        if (item.inventorySlotReceiver._weaponHost != null)
+                        {
+                            var poolee = item.inventorySlotReceiver._weaponHost.GetTransform().GetComponent<Poolee>();
+                            if (poolee != null)
+                            {
+                                var barcode = poolee.SpawnableCrate.Barcode;
+                                slots.Add(item.name, barcode);
+                            }
+                        }
+
+                    }
+                }
+                if (IsDebug) LoggerInstance.Msg("Successfully saved inventory");
+            }
+        }
+
+        private void LevelLoadedEvent(LevelInfo obj)
+        {
+            if (Settings.Enabled)
+            {
+                 LoggerInstance.Msg("Loading inventory...");
+                if (slots.Count >= 1)
+                {
+                    var list = Player.RigManager.inventory.bodySlots.ToList();
+                    foreach (var item in slots)
+                    {
+                        var slot = list.Find((slot) => slot.name == item.Key);
+                        if (slot != null){
+                            if(slot.inventorySlotReceiver != null)
+                            {
+                                slot.inventorySlotReceiver.SpawnInSlotAsync(item.Value);
+                            }
+                        }
+                    }
+                    LoggerInstance.Msg(" Loaded inventory");
+                }
+                else
+                {
+                    LoggerInstance.Msg("Saved Inventory not found");
+                }
+            }
+            else
+            {
+                if (IsDebug) LoggerInstance.Msg("[DEBUG] Saving/Loading Inventory has been disabled, check the mod settings to enable again");
+            }
+        }
+
+        public void SetupMenu()
+        {
+            var mainPage = Page.Root.CreatePage("HAHOOS", Color.white);
+            var modPage = mainPage.CreatePage("KeepInventory", new Color(255, 72, 59));
+            modPage.CreateBool("Enabled", Color.green, Settings.Enabled, (value)=>Settings.Enabled=value);
+        }
+    }
+}
---
+++ KeepInventory/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAHOOS.KeepInventory
+{
+    internal static class Settings
+    {
+        public static bool Enabled { get; set; } = true;
+    }
+}
