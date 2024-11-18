1a1c2386ba5c4bbb140008d720b8eefd1c04347d
HAHOOS <hahoos@hahoos.pl>
2024-11-18 21:58:09 +0100

    Fixed the code creating duplicates of entries, the title in notifications being the message

--- KeepInventory/Helper/BLHelper.cs
+++ KeepInventory/Helper/BLHelper.cs
@@ -1,9 +1,9 @@
 ﻿using BoneLib;
 using BoneLib.BoneMenu;
-using MelonLoader;
-using UnityEngine;
-using System;
 using BoneLib.Notifications;
+using MelonLoader;
+using System;
+using UnityEngine;
 
 namespace KeepInventory.Helper
 {
@@ -19,11 +19,13 @@ namespace KeepInventory.Helper
 
         internal static IntElement CreateIntPref(this Page page, string name, Color color, ref MelonPreferences_Entry<int> value, int increment, int minValue, int maxValue, Action<int> callback = null, string prefName = null, int prefDefaultValue = default)
         {
-            prefName ??= name;
+            if (value == null || !prefs.HasEntry(value.Identifier))
+            {
+                prefName ??= name;
 
-            if (!prefs.HasEntry(prefName))
-                value = prefs.CreateEntry(prefName, prefDefaultValue);
-
+                if (!prefs.HasEntry(prefName))
+                    value = prefs.CreateEntry(prefName, prefDefaultValue);
+            }
             MelonPreferences_Entry<int> val = value;
             return page.CreateInt(name, color, val.Value, increment, minValue, maxValue, (x) =>
             {
@@ -35,11 +37,13 @@ namespace KeepInventory.Helper
 
         internal static FloatElement CreateFloatPref(this Page page, string name, Color color, ref MelonPreferences_Entry<float> value, float increment, float minValue, float maxValue, Action<float> callback = null, string prefName = null, float prefDefaultValue = default)
         {
-            prefName ??= name;
+            if (value == null || !prefs.HasEntry(value.Identifier))
+            {
+                prefName ??= name;
 
-            if (!prefs.HasEntry(prefName))
-                value = prefs.CreateEntry(prefName, prefDefaultValue);
-
+                if (!prefs.HasEntry(prefName))
+                    value = prefs.CreateEntry(prefName, prefDefaultValue);
+            }
             MelonPreferences_Entry<float> val = value;
             return page.CreateFloat(name, color, val.Value, increment, minValue, maxValue, (x) =>
             {
@@ -51,11 +55,13 @@ namespace KeepInventory.Helper
 
         internal static BoolElement CreateBoolPref(this Page page, string name, Color color, ref MelonPreferences_Entry<bool> value, Action<bool> callback = null, string prefName = null, bool prefDefaultValue = default)
         {
-            prefName ??= name;
+            if (value == null || !prefs.HasEntry(value.Identifier))
+            {
+                prefName ??= name;
 
-            if (!prefs.HasEntry(prefName))
-                value = prefs.CreateEntry(prefName, prefDefaultValue);
-
+                if (!prefs.HasEntry(prefName))
+                    value = prefs.CreateEntry(prefName, prefDefaultValue);
+            }
             MelonPreferences_Entry<bool> val = value;
             return page.CreateBool(name, color, val.Value, (x) =>
             {
@@ -67,11 +73,13 @@ namespace KeepInventory.Helper
 
         internal static EnumElement CreateEnumPref<T>(this Page page, string name, Color color, ref MelonPreferences_Entry<T> value, Action<Enum> callback = null, string prefName = null, Enum prefDefaultValue = default) where T : Enum
         {
-            prefName ??= name;
+            if (value == null || !prefs.HasEntry(value.Identifier))
+            {
+                prefName ??= name;
 
-            if (!prefs.HasEntry(prefName))
-                value = prefs.CreateEntry(prefName, (T)prefDefaultValue);
-
+                if (!prefs.HasEntry(prefName))
+                    value = prefs.CreateEntry(prefName, (T)prefDefaultValue);
+            }
             MelonPreferences_Entry<T> val = value;
             return page.CreateEnum(name, color, val.Value, (x) =>
             {
@@ -83,11 +91,13 @@ namespace KeepInventory.Helper
 
         internal static StringElement CreateStringPref(this Page page, string name, Color color, ref MelonPreferences_Entry<string> value, Action<string> callback = null, string prefName = null, string prefDefaultValue = default)
         {
-            prefName ??= name;
+            if (value == null || !prefs.HasEntry(value.Identifier))
+            {
+                prefName ??= name;
 
-            if (!prefs.HasEntry(prefName))
-                value = prefs.CreateEntry(prefName, prefDefaultValue);
-
+                if (!prefs.HasEntry(prefName))
+                    value = prefs.CreateEntry(prefName, prefDefaultValue);
+            }
             MelonPreferences_Entry<string> val = value;
             StringElement element = page.CreateString(name, color, val.Value, (x) =>
             {
@@ -103,7 +113,7 @@ namespace KeepInventory.Helper
 
         #region Notifications
 
-        public static void SendNotification(string message, string title = null, bool showTitleOnPopup = false, float popupLength = 2f, NotificationType type = NotificationType.Information, Texture2D customIcon = null)
+        public static void SendNotification(string title, string message, bool showTitleOnPopup = false, float popupLength = 2f, NotificationType type = NotificationType.Information, Texture2D customIcon = null)
         {
             if (Core.mp_showNotifications?.Value != true) return;
             Notifier.Send(new Notification()
@@ -117,12 +127,13 @@ namespace KeepInventory.Helper
             });
         }
 
-        public static void SendNotification(NotificationText message, NotificationText title, bool showTitleOnPopup = false, float popupLength = 2f, NotificationType type = NotificationType.Information, Texture2D customIcon = null)
+        public static void SendNotification(NotificationText title, NotificationText message, bool showTitleOnPopup = false, float popupLength = 2f, NotificationType type = NotificationType.Information, Texture2D customIcon = null)
         {
+            title.Text = $"KeepInventory | {title.Text}";
             if (Core.mp_showNotifications?.Value != true) return;
             Notifier.Send(new Notification()
             {
-                Title = $"KeepInventory | {title}",
+                Title = title,
                 Message = message,
                 ShowTitleOnPopup = showTitleOnPopup,
                 CustomIcon = customIcon,
